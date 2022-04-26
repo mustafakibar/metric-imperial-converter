8f9fee6284aa7a27ec80acbefaa0696c6687f079
kibar <mustafa@kibar.pro>
2022-04-26 22:07:25 +0300

    Fix minor typos in code

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,27 +12,31 @@ const convertHandler = new ConvertHandler();
 
 suite('Unit Tests', (done) => {
   suite('Validate Input Number', () => {
-    test('should read the numbers in the input', (done) => {
+    test('Should read the numbers in the input', (done) => {
       assert.equal(convertHandler.getNum('5kg'), 5);
       assert.equal(convertHandler.getNum('34lbs'), 34);
       done();
     });
-    test('should read the decimal numbers in the input', (done) => {
+
+    test('Should read the decimal numbers in the input', (done) => {
       assert.equal(convertHandler.getNum('5.3kg'), 5.3);
       assert.equal(convertHandler.getNum('34.13lbs'), 34.13);
       done();
     });
-    test('should read the fractial in the input', (done) => {
+
+    test('Should read the fractial in the input', (done) => {
       assert.equal(convertHandler.getNum('3/5kg'), 0.6);
       assert.equal(convertHandler.getNum('13/34lbs'), 0.38235);
       done();
     });
-    test('should read the fractial with decimal in the input', (done) => {
+
+    test('Should read the fractial with decimal in the input', (done) => {
       assert.equal(convertHandler.getNum('3.1/5kg'), 0.62);
       assert.equal(convertHandler.getNum('13.1/34lbs'), 0.38529);
       done();
     });
-    test('should correctly return an error due to double fraction', (done) => {
+
+    test('Should correctly return an error due to double fraction', (done) => {
       assert.equal(convertHandler.getNum('3./1//5kg'), ERROR_INVALID_NUMBER);
       assert.equal(
         convertHandler.getNum('13.1/34/13.1lbs'),
@@ -40,7 +44,8 @@ suite('Unit Tests', (done) => {
       );
       done();
     });
-    test('should correctly return 1 when no number provided', (done) => {
+
+    test('Should correctly return 1 when no number provided', (done) => {
       assert.equal(convertHandler.getNum('kg'), 1);
       assert.equal(convertHandler.getNum('lbs'), 1);
       done();
@@ -48,17 +53,19 @@ suite('Unit Tests', (done) => {
   });
 
   suite('Validate Input Unit', () => {
-    test('should read the unit in the input', (done) => {
+    test('Should read the unit in the input', (done) => {
       assert.equal(convertHandler.getUnit('5kg'), 'kg');
       assert.equal(convertHandler.getUnit('34lbs'), 'lbs');
       done();
     });
-    test('should correctly return an error due to invalid input unit', (done) => {
+
+    test('Should correctly return an error due to invalid input unit', (done) => {
       assert.startsWith(convertHandler.getUnit('5.3k'), ERROR_INVALID_UNIT);
       assert.startsWith(convertHandler.getUnit('34.13'), ERROR_INVALID_UNIT);
       done();
     });
-    test('should correctly return the correct return unit', (done) => {
+
+    test('Should correctly return the correct return unit', (done) => {
       assert.equal(convertHandler.getReturnUnit('lbs'), 'kg');
       assert.equal(convertHandler.getReturnUnit('kg'), 'lbs');
       assert.equal(convertHandler.getReturnUnit('mi'), 'km');
@@ -67,7 +74,8 @@ suite('Unit Tests', (done) => {
       assert.equal(convertHandler.getReturnUnit('L'), 'gal');
       done();
     });
-    test('should correctly return the spelled-out string unit', (done) => {
+
+    test('Should correctly return the spelled-out string unit', (done) => {
       assert.equal(
         convertHandler.getString(1, 'kg', 2.20462262, 'lbs'),
         '1 kilograms converts to 2.20462262 pounds'
@@ -80,38 +88,38 @@ suite('Unit Tests', (done) => {
     });
   });
 
-  suite('validate Convertion', () => {
-    test('should correctly convert km to mi', (done) => {
+  suite('Validate Convertion', () => {
+    test('Should correctly convert km to mi', (done) => {
       assert.equal(convertHandler.convert(1, 'km'), 0.62137);
       assert.equal(convertHandler.convert(34.13, 'km'), 21.20745);
       done();
     });
 
-    test('should correctly convert mi to km', (done) => {
+    test('Should correctly convert mi to km', (done) => {
       assert.equal(convertHandler.convert(1, 'mi'), 1.60934);
       assert.equal(convertHandler.convert(34.13, 'mi'), 54.92677);
       done();
     });
 
-    test('should correctly convert kg to lbs', (done) => {
+    test('Should correctly convert kg to lbs', (done) => {
       assert.equal(convertHandler.convert(1, 'kg'), 2.20462);
       assert.equal(convertHandler.convert(34.13, 'kg'), 75.24383);
       done();
     });
 
-    test('should correctly convert lbs to kg', (done) => {
+    test('Should correctly convert lbs to kg', (done) => {
       assert.equal(convertHandler.convert(1, 'lbs'), 0.45359);
       assert.equal(convertHandler.convert(34.13, 'lbs'), 15.48109);
       done();
     });
 
-    test('should correctly convert gal to L', (done) => {
+    test('Should correctly convert gal to L', (done) => {
       assert.equal(convertHandler.convert(1, 'gal'), 3.78541);
       assert.equal(convertHandler.convert(34.13, 'gal'), 129.19604);
       done();
     });
 
-    test('should correctly convert L to gal', (done) => {
+    test('Should correctly convert L to gal', (done) => {
       assert.equal(convertHandler.convert(1, 'L'), 0.26417);
       assert.equal(convertHandler.convert(34.13, 'L'), 9.0162);
       done();
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -12,85 +12,75 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
   suite('GET /api/convert', function () {
-    {
-      const input = '5.3kg';
-      test(`Convert valid input: ${input}`, (done) => {
-        chai
-          .request(server)
-          .get('/api/convert')
-          .query({ input })
-          .end((_err, res) => {
-            assert.equal(res.status, 200);
-            assert.equal(res.body.initNum, 5.3);
-            assert.equal(res.body.initUnit, 'kg');
-            assert.approximately(res.body.returnNum, 11.68451, 0.1);
-            assert.equal(res.body.returnUnit, 'lbs');
-            done();
-          });
-      });
-    }
+    const validInput = '5.3kg';
+    test(`Convert valid input: ${validInput}`, (done) => {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: validInput })
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 5.3);
+          assert.equal(res.body.initUnit, 'kg');
+          assert.approximately(res.body.returnNum, 11.68451, 0.1);
+          assert.equal(res.body.returnUnit, 'lbs');
+          done();
+        });
+    });
 
-    {
-      const input = '34.13xlbs';
-      test(`Convert invalid input: ${input}`, function (done) {
-        chai
-          .request(server)
-          .get('/api/convert')
-          .query({ input })
-          .end((_err, res) => {
-            assert.equal(res.status, 200);
-            assert.equal(res.body, ERROR_INVALID_UNIT);
-            done();
-          });
-      });
-    }
+    const invalidInput = '34.13xlbs';
+    test(`Convert invalid input: ${invalidInput}`, function (done) {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: invalidInput })
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, ERROR_INVALID_UNIT);
+          done();
+        });
+    });
 
-    {
-      const input = '34/1/3lbs';
-      test(`Convert invalid number: ${input}`, (done) => {
-        chai
-          .request(server)
-          .get('/api/convert')
-          .query({ input })
-          .end((_err, res) => {
-            assert.equal(res.status, 200);
-            assert.equal(res.body, ERROR_INVALID_NUMBER);
-            done();
-          });
-      });
-    }
+    const invalidNumber = '34/1/3lbs';
+    test(`Convert invalid number: ${invalidNumber}`, (done) => {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: invalidNumber })
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, ERROR_INVALID_NUMBER);
+          done();
+        });
+    });
 
-    {
-      const input = '34/1/3xlbs';
-      test(`Convert invalid number and unit: ${input}`, (done) => {
-        chai
-          .request(server)
-          .get('/api/convert')
-          .query({ input })
-          .end((_err, res) => {
-            assert.equal(res.status, 200);
-            assert.equal(res.body, ERROR_INVALID_NUMBER + ' and unit');
-            done();
-          });
-      });
-    }
+    const invalidNumberAndUnit = '34/1/3xlbs';
+    test(`Convert invalid number and unit: ${invalidNumberAndUnit}`, (done) => {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: invalidNumberAndUnit })
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, ERROR_INVALID_NUMBER + ' and unit');
+          done();
+        });
+    });
 
-    {
-      const input = 'lbs';
-      test(`Convert with no number: ${input}`, (done) => {
-        chai
-          .request(server)
-          .get('/api/convert')
-          .query({ input })
-          .end((_err, res) => {
-            assert.equal(res.status, 200);
-            assert.equal(
-              res.body.string,
-              '1 pounds converts to 0.45359 kilograms'
-            );
-            done();
-          });
-      });
-    }
+    const inputWithoutNumber = 'lbs';
+    test(`Convert with no number: ${inputWithoutNumber}`, (done) => {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: inputWithoutNumber })
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(
+            res.body.string,
+            '1 pounds converts to 0.45359 kilograms'
+          );
+          done();
+        });
+    });
   });
 });
